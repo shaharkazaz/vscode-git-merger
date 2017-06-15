b55810e8f6e6ee0f279a58d9d1bc887ee68405c9
shaharkazaz <shahar.kazaz@datorama.com>
2017-06-15 22:27:43 +0300

    * Implemented a simple working example

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec, execSync } from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,12 +11,28 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.gitMerge', () => {
+    let disposable = vscode.commands.registerCommand('extension.gitMergeFrom', () => {
         // The code you place here will be executed every time your command is executed
-
-        if(vscode.workspace.getConfiguration('git').get('path')){
-            console.log(<string>vscode.workspace.getConfiguration('git').get('path'));
-        }
+        exec('git branch -a',{cwd: vscode.workspace.rootPath}, (error, stdout, stderr) => {
+            if(error){
+                vscode.window.showErrorMessage(error.toString());
+                return;
+            }
+            let listOfBranches = stdout.split("\n").filter((branch) => {
+                return (branch.trim().length > 0 && branch.indexOf("*") == -1)
+            });
+            vscode.window.showQuickPick(listOfBranches, "Chose destination branch").then(chosenitem => {
+                if(chosenitem){
+                    
+                    exec("git merge " + chosenitem + "--no-commit --no-ff", {cwd: vscode.workspace.rootPath}, (error, stdout, stderr) => {
+                        if(error){
+                            vscode.window.showErrorMessage(error.toString());
+                            return;
+                        }
+                    });
+                }
+            });
+        });
     });
 
     context.subscriptions.push(disposable);
