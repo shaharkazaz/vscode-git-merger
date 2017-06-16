e6f22e5d3401a5bf9c3d9c04ba222e71d8fea29f
shaharkazaz <shahar.kazaz@datorama.com>
2017-06-16 11:53:06 +0300

    * Added extention icon
    * Updated merge function
    * Added abort function
    * Updated README file
    * Added extention icon

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # vscode-git-merger
 
-No more terminal! Merging branches now in vscode.
+No more terminal! Merging branches now in vscode.  
+An easy merge flow using the vscode conflict handler.
+
+# Road Map
+
+* Easy and intuative merging.  
+* Integrated conflict handling.
+
+# Relase Notes
+
+v0.0.1 - A very limited and simple implimation of the extention.
--- src/extension.ts
+++ src/extension.ts
@@ -8,34 +8,50 @@ import { exec, execSync } from 'child_process';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.gitMergeFrom', () => {
-        // The code you place here will be executed every time your command is executed
         exec('git branch -a',{cwd: vscode.workspace.rootPath}, (error, stdout, stderr) => {
             if(error){
                 vscode.window.showErrorMessage(error.toString());
                 return;
             }
-            let listOfBranches = stdout.split("\n").filter((branch) => {
+            let currentBranch,
+            listOfBranches = stdout.split("\n").filter((branch) => {
+                if(branch.indexOf("*") != -1){
+                    currentBranch = branch.replace('*','').trim();
+                    return false;
+                }
                 return (branch.trim().length > 0 && branch.indexOf("*") == -1)
             });
             vscode.window.showQuickPick(listOfBranches, "Chose destination branch").then(chosenitem => {
                 if(chosenitem){
-                    
-                    exec("git merge " + chosenitem + "--no-commit --no-ff", {cwd: vscode.workspace.rootPath}, (error, stdout, stderr) => {
+                    exec("git merge " + chosenitem + " --no-commit --no-ff", {cwd: vscode.workspace.rootPath}, (error, stdout, stderr) => {
                         if(error){
-                            vscode.window.showErrorMessage(error.toString());
+                            console.log("Error while mergning");
+                            console.log(error);
+                            vscode.window.showErrorMessage("Oops! something didn't work check the log for more inforamtion");
                             return;
                         }
+                        vscode.window.setStatusBarMessage(chosenitem + " was merged into" + currentBranch);
                     });
                 }
             });
         });
     });
+    let disposable2 = vscode.commands.registerCommand('extension.gitAbortMerge', () => {
+        // The code you place here will be executed every time your command is executed
+        exec('git merge --abort',{cwd: vscode.workspace.rootPath}, (error, stdout, stderr) => {
+            if(error){
+                console.log("Error while mergning");
+                console.log(stderr.replace("fatal:", ""));
+                vscode.window.showErrorMessage("Oops! something didn't work check the log for more inforamtion");
+                return;
+            }
+            vscode.window.setStatusBarMessage("Merge was successfully aborted");
+        });
+    });
 
     context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
