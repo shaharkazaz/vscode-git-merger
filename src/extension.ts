4ed3be9f09ea17bb3337cf2f0feff69e63286fce
shaharkazaz <shahar.kazaz@datorama.com>
2017-06-18 13:40:28 +0300

    * added condlifct handler * added up to date handler

--- src/extension.ts
+++ src/extension.ts
@@ -15,23 +15,41 @@ export function activate(context: vscode.ExtensionContext) {
                 return;
             }
             let currentBranch,
-            listOfBranches = stdout.split("\n").filter((branch) => {
+            branchesNames = stdout.split("\n").filter((branch) => {
                 if(branch.indexOf("*") != -1){
                     currentBranch = branch.replace('*','').trim();
                     return false;
                 }
                 return (branch.trim().length > 0 && branch.indexOf("*") == -1)
             });
-            vscode.window.showQuickPick(listOfBranches, "Chose destination branch").then(chosenitem => {
+            vscode.window.showQuickPick(branchesNames, "Chose destination branch").then(chosenitem => {
                 if(chosenitem){
                     exec("git merge " + chosenitem + " --no-commit --no-ff", {cwd: vscode.workspace.rootPath}, (error, stdout, stderr) => {
-                        if(error){
+                        if(stdout){
+                            if(stdout.toLowerCase().indexOf("conflict") != -1){
+                                let conflictedFiles = stdout.split("\n"),conflictedFilesLength = conflictedFiles.length -1;
+                                console.log("Conflicts while mergning in the following files:");
+                                for(let i = 0; i < conflictedFilesLength; i++){
+                                    let conflictIndex = conflictedFiles[i].indexOf("CONFLICT (content): Merge conflict in");
+                                    if(conflictIndex != -1){
+                                        console.log(conflictedFiles[i].substr(38, conflictedFiles[i].length));
+                                    }
+                                }
+                                vscode.window.showWarningMessage("Seems like there are some conflicts to handle (Conflicted files list in log)");
+                                return;
+                            }
+                            else if(stdout.indexOf("up-to-date") != -1){
+                                vscode.window.setStatusBarMessage("Already up-to-date", 3000);
+                                return;
+                            }
+                        }
+                        else if(error){
                             console.log("Error while mergning");
                             console.log(error);
                             vscode.window.showErrorMessage("Oops! something didn't work check the log for more inforamtion");
                             return;
                         }
-                        vscode.window.setStatusBarMessage(chosenitem + " was merged into" + currentBranch);
+                        vscode.window.setStatusBarMessage(chosenitem + " was merged into " + currentBranch, 3000);
                     });
                 }
             });
@@ -46,7 +64,7 @@ export function activate(context: vscode.ExtensionContext) {
                 vscode.window.showErrorMessage("Oops! something didn't work check the log for more inforamtion");
                 return;
             }
-            vscode.window.setStatusBarMessage("Merge was successfully aborted");
+            vscode.window.setStatusBarMessage("Merge was successfully aborted", 3000);
         });
     });
 
