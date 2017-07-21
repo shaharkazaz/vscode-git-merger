6b197e1e7e4d122df2c4473fb6cf9b0a18c819ad
shaharkazaz <shahar.kazaz@datorama.com>
2017-07-21 15:08:50 +0300

    * Added a map for the allowed git merge commands

---
+++ src/constants/allowedMergeFlags.ts
@@ -0,0 +1,19 @@
+export const allowedMergeFlags = {
+    "commit": "--",
+    "no-commit": "--",
+    "ff": "--",
+    "no-ff": "--",
+    "ff-only": "--",
+    "log": "--",
+    "stat": "--",
+    "n": "-",
+    "no-stat": "--",
+    "squash": "--",
+    "no-squash": "--",
+    "s": "-",
+    "strategy": "--",
+    "X": "-",
+    "strategy-option": "--",
+    "verify-signatures": "--",
+    "no-verify-signatures": "--"
+}
